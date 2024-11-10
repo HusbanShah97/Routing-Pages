7098d1f28e22e1386ed8599afd81b902a13f7406
HusbanShah97 <husbanshah97@gmail.com>
2024-11-10 15:05:22 +0500

    complete assignment

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,29 +1,36 @@
 
 import Link from "next/link";
-export default function Navbar(){
+export default function Navbar() {
 
     return (
+
+
         
-                   
-        <div>
-        <div className="flex items-center justify-end w-full h-24 gap-5 mr-10 font-bold bg-blue-600" >
+        
+            <nav className="flex items-center justify-end w-full h-24 gap-7 pr-40 font-bold bg-blue-600" >
+            <div className="ml-32 flex justify-start w-full">
                 <div>
-           <h1 className="">Husban Tech</h1>
-                </div>           
-            <Link href={"/"} className="mr-5 border-black">Home</Link>
-            <Link href="/about">About</Link><br />
-            <Link href="/contact">Contact</Link><br />
-            <Link href="/product">Product</Link><br />
-            <Link href="/project">Project</Link><br />
-            <Link href="/courses">Courses</Link><br />
+                    <h1 className="font-serif ">Husban Tech</h1>
+                </div>
+              </div>
             
-       </div>
-        </div>
+
+
+                <Link href={"/"} className="mr-5 ">Home</Link>
+                <Link href="/about">About</Link><br />
+                <Link href="/contact">Contact</Link><br />
+                <Link href="/product">Product</Link><br />
+                <Link href="/project">Project</Link><br />
+                <Link href="/courses">Courses</Link><br />
+          
+            
+            </nav>
+        
     )
 
 
 
 
 
-   
+
 }
